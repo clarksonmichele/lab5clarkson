b4c802fa177e2d2a88ffed8f6b59aafd001d430a
micheleclarkson <micheleclarkson@rogers.com>
2016-03-15 14:55:42 -0400

    lab5
    CRUD with mongoDb and Heroku

---
+++ config/db.js
@@ -0,0 +1,9 @@
+// create public connection string
+module.exports = {
+
+    // local
+    // 'url': 'mongodb://localhost/test'
+
+    // live on mLab
+    'url': 'mongodb://mclarkson:1234qwer@ds064748.mlab.com:64748/lab5'
+};
---
+++ public/javascripts/scripts.js
@@ -0,0 +1,4 @@
+//delete confirmation
+$('.confirmation').on('click', function() {
+	return confirm('Are you sure that you want to delete this article?');
+});
