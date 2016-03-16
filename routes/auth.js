70a4d7e8212f5f06fe83bd715ced8a8debf4994c
micheleclarkson <micheleclarkson@rogers.com>
2016-03-16 10:33:12 -0400

    Invalid Login

--- routes/auth.js
+++ routes/auth.js
@@ -30,9 +30,21 @@ router.post('/register', function(req, res, next) {
 //GET login and display the form
 router.get('/login', function(req, res, next) {
 	res.render('auth/login', {
-		title: 'Login'
+		title: 'Login',
+		user: req.user,
+		massages: req.sesssion.messages || []
 	});
+
+	//clear out session messages so they don't just build up 
+	req.session.messages = [];
 });
+
+//POST login - validate the user and send them to articles or back to login
+router.post('/login', passport.authenticate('local', {
+   successRedirect: '/articles',
+   failureRedirect: '/auth/login',
+   failureMessage: 'Invalid Login'
+}));
 
 //Make the file public
 module.exports = router;
