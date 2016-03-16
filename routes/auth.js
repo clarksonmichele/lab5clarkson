e0c3ec1c890787293ac4bbc3b05deaefd9b10c7b
micheleclarkson <micheleclarkson@rogers.com>
2016-03-16 10:13:39 -0400

    Wednesday Commit 2
    saving change to registration. Remove “return”.

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+//add auth packages
+var passport = require('passport');
+var mongoose = require('mongoose');
+var Account = require('../models/account');
+
 //GET register and display the form
 router.get('/register', function(req, res, next) {
 	res.render('auth/register', {
 		title: 'Register'
 	});
+});
+
+//POST register and display the form
+router.post('/register', function(req, res, next) {
+	Account.register(new Account({ username: req.body.username }), req.body.password, function(err, account) {
+        if (err) {
+           res.render('auth/register', { title: 'Register' });
+        }
+        else {
+            req.login(account, function(err) {
+                res.redirect('/articles');
+            });
+        }
+    });
 });
 
 //GET login and display the form
